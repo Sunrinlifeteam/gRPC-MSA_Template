6f23d06be85a8a71817db72024842adfcad60365
rmagur1203 <rmagur1203@naver.com>
2022-09-13 11:42:26 +0900

    test: add e2e test

--- packages/hello/test/app.e2e-spec.ts
+++ packages/hello/test/app.e2e-spec.ts
@@ -1,24 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, INestMicroservice } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { grpcClientOptions } from 'shared/lib/options/hello.grpc';
+import * as ProtoLoader from '@grpc/proto-loader';
+import * as GRPC from '@grpc/grpc-js';
+import { Hello } from 'shared/lib/generated/hello';
+import { ServiceClient } from '@grpc/grpc-js/build/src/make-client';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+  let server;
+  let app: INestMicroservice;
+  let client: ServiceClient;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleFixture.createNestMicroservice(grpcClientOptions);
+
     await app.init();
+
+    const proto = ProtoLoader.loadSync(grpcClientOptions.options.protoPath);
+    const protoGRPC = GRPC.loadPackageDefinition(proto) as any;
+    const serviceConstructor: GRPC.ServiceClientConstructor =
+      protoGRPC.hello.HelloService;
+    client = new serviceConstructor(
+      grpcClientOptions.options.url,
+      GRPC.credentials.createInsecure(),
+    );
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('getHello (gRPC)', async () => {
+    return new Promise<void>((resolve, reject) => {
+      client.getHello({}, function (err, res) {
+        if (err) return resolve(err);
+        expect(res).toEqual({ message: 'Hello World!' });
+        resolve();
+      });
+      setTimeout(() => resolve(), 1000);
+    });
+  });
+
+  afterAll(async () => {
+    client.close();
+    await app.close();
   });
 });
